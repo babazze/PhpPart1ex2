95d69a3d415630f7409ee2225a71422332d2e0a6
babazze <babazze@live.fr>
2019-12-02 16:04:12 +0100

    go

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+$lastname = 'Att ';
+$firstname = 'Azz ';
+$age = 26;
+?>
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8" />
+    <title>ex2</title>
+  </head>
+  <body>
+      <p><?= $lastname; ?><?= $firstname; ?><?= $age; ?></p>
+  </body>
+</html>
